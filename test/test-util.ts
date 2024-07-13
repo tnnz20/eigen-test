d58df24b4f7d3fbc8fb4d492904baeebaa63b417
tnnz20 <kaitora200@gmail.com>
2024-07-13 17:16:19 +0800

    test: add new test util

--- test/test-util.ts
+++ test/test-util.ts
@@ -18,4 +18,27 @@ export class MemberTest {
             },
         });
     }
+
+    static async createMany() {
+        await prismaClient.member.createMany({
+            data: [
+                {
+                    code: "T-001",
+                    name: "Jane Doe",
+                    created_at: new Date().getTime(),
+                    updated_at: new Date().getTime(),
+                },
+                {
+                    code: "T-002",
+                    name: "John Smith",
+                    created_at: new Date().getTime(),
+                    updated_at: new Date().getTime(),
+                },
+            ],
+        });
+    }
+
+    static async deleteAll() {
+        await prismaClient.member.deleteMany();
+    }
 }
