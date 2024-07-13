a3857bcda0ef231a4a7d2859f456d2a1e047f7d5
tnnz20 <kaitora200@gmail.com>
2024-07-13 17:14:30 +0800

    feat: add validation get members

--- src/validation/member-validation.ts
+++ src/validation/member-validation.ts
@@ -5,4 +5,11 @@ export class MemberValidation {
         code: z.string().min(1).max(10),
         name: z.string().min(1).max(100),
     });
+
+    static readonly GET_MEMBERS: ZodType = z.object({
+        code: z.string().min(1).max(10).optional(),
+        name: z.string().min(3).max(10).optional(),
+        size: z.number().int().min(1).positive(),
+        page: z.number().int().min(1).positive(),
+    });
 }
