1c5ea306308cbada049a47202c4f2fa559b29a30
tnnz20 <kaitora200@gmail.com>
2024-07-13 13:11:26 +0800

    feat: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    endOfLine: "lf",
+    semi: true,
+    singleQuote: false,
+    tslintIntegration: true,
+    eslintIntegration: true,
+    tabWidth: 4,
+    trailingComma: "es5",
+}
