7fa35e8d9201b7968c0456cf855bf9c8f734adca
tnnz20 <kaitora200@gmail.com>
2024-07-13 22:47:16 +0800

    test: add unit test to borrow book

--- test/book.test.ts
+++ test/book.test.ts
@@ -4,7 +4,7 @@ import {
     ErrorResponseAPI,
     SuccessResponseAPI,
 } from "../src/response/response_constant";
-import { BookTest } from "./test-util";
+import { BookTest, MemberTest } from "./test-util";
 
 describe("POST /api/v1/books", () => {
     afterAll(async () => {
@@ -64,5 +64,69 @@ describe("GET /api/v1/books", () => {
         expect(response.status).toBe(200);
         expect(response.body.data).toBeDefined();
         expect(response.body.paging).toBeDefined();
+    });
+});
+
+describe("POST /api/v1/books/borrow", () => {
+    beforeAll(async () => {
+        await MemberTest.create();
+        await BookTest.createMany();
+    });
+
+    afterAll(async () => {
+        await BookTest.deleteAllBorrow();
+        await MemberTest.delete();
+        await BookTest.deleteAll();
+    });
+
+    it("should reject borrow book if request is invalid", async () => {
+        const response = await supertest(app)
+            .post("/api/v1/books/borrow")
+            .send({
+                memberCode: "",
+                bookCode: "",
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toBeDefined();
+        expect(response.body.message).toEqual(ErrorResponseAPI.BadRequest);
+    });
+
+    it("should reject borrow book if book is not found", async () => {
+        const response = await supertest(app)
+            .post("/api/v1/books/borrow")
+            .send({
+                memberCode: "M-001",
+                bookCode: "B-002",
+            });
+
+        expect(response.status).toBe(404);
+        expect(response.body.errors).toBeDefined();
+        expect(response.body.message).toEqual(ErrorResponseAPI.NotFound);
+    });
+
+    it("should reject borrow book if already borrowed", async () => {
+        const response = await supertest(app)
+            .post("/api/v1/books/borrow")
+            .send({
+                memberCode: "T-001",
+                bookCode: "B-002",
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toBeDefined();
+        expect(response.body.message).toEqual(ErrorResponseAPI.BadRequest);
+    });
+
+    it("should borrow book", async () => {
+        const response = await supertest(app)
+            .post("/api/v1/books/borrow")
+            .send({
+                memberCode: "T-001",
+                bookCode: "B-001",
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body.message).toEqual(SuccessResponseAPI.Created);
     });
 });
