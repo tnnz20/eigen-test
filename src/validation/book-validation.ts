628f544880b3c60b8f012c2eb6c46ba637b69d23
tnnz20 <kaitora200@gmail.com>
2024-07-13 19:33:24 +0800

    feat: add get books validation

--- src/validation/book-validation.ts
+++ src/validation/book-validation.ts
@@ -2,8 +2,17 @@ import { z, ZodType } from "zod";
 export class BookValidation {
     static readonly CREATE: ZodType = z.object({
         code: z.string().min(1).max(10),
-        title: z.string().min(1).max(100),
-        author: z.string().min(1).max(100),
+        title: z.string().min(3).max(100),
+        author: z.string().min(3).max(100),
         stock: z.number().int().min(1),
+    });
+
+    static readonly GET_BOOKS: ZodType = z.object({
+        code: z.string().min(1).max(10).optional(),
+        title: z.string().min(3).max(100).optional(),
+        author: z.string().min(3).max(100).optional(),
+        stock: z.number().int().min(1).optional(),
+        size: z.number().int().min(1).positive(),
+        page: z.number().int().min(1).positive(),
     });
 }
