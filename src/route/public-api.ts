bd068fc92535ec33bb7b14f2dc164495aa3942cc
tnnz20 <kaitora200@gmail.com>
2024-07-13 18:09:45 +0800

    refactor: create chainable route using route

--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -2,9 +2,12 @@ import express from "express";
 import { MemberController } from "../domains/member/member-controller";
 
 export const publicRouter = express.Router();
-publicRouter.post("/api/v1/members", MemberController.create);
-publicRouter.get("/api/v1/members", MemberController.getMembers);
-publicRouter.get(
-    "/api/v1/members/:memberCode",
-    MemberController.getMemberByCode
-);
+
+publicRouter
+    .route("/api/v1/members")
+    .post(MemberController.create)
+    .get(MemberController.getMembers);
+
+publicRouter
+    .route("/api/v1/members/:memberCode")
+    .get(MemberController.getMemberByCode);
