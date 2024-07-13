78bb637d9ebddbfe01213cc759c2c8b4763dbb9c
tnnz20 <kaitora200@gmail.com>
2024-07-13 13:52:42 +0800

    feat: add create member route

---
+++ src/route/public-api.ts
@@ -0,0 +1,5 @@
+import express from "express";
+import { MemberController } from "../domains/member/member-controller";
+
+export const publicRouter = express.Router();
+publicRouter.post("/api/v1/members", MemberController.create);
